64ea4859371ef61f608b6dbe7793fb15114d117b
suarem298 <suarem298@gmail.com>
2020-08-18 19:43:18 -0400

    Create codeWarChallenge.js

---
+++ codeWarChallenge.js
@@ -0,0 +1,21 @@
+//Your goal is make a function that finds the difference between two arrays
+//and only return the unique ones
+function arrayDiff(a, b) {
+    let holder=[];
+   
+      for(let x=0;x<a.length;x++)
+    {
+      if(!b.includes(a[x]))
+        {
+            holder.push(a[x]);
+        }
+    }
+    
+   return holder;
+ }
+ console.log(arrayDiff(([1,8,2], [])));
+
+//Take a input name and return a greeting.
+ function greet(name){
+    return `Hello, ${name} how are you doing today?`
+   }
